fc9c7a5666a371346d94f50f28a48833ee43a5af
joaofilipigtissi <joaofilipigtissi@gmail.com>
2023-06-27 12:10:42 -0300

    vsfd

--- carrinho.php
+++ carrinho.php
@@ -3,107 +3,49 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carrinho de compras</title>
-    <style type="text/css">
-        *{margin: 0; padding: 0; box-sizing: border-box;}
-        h2.title{
-            background-color: #069;
-            width: 100%;
-            padding: 20px;
-            text-align: center;
-            color: white;
-        }
-
-        .carrinho-container{
-            display: flex; 
-            margin-top: 10px;           
-        }
-
-        .produto{
-            width: 25%;
-            padding: 0 30px;
-        }
-        .produto img {
-            max-width: 100%
-        }
-        .produto a{
-            display: block;
-            width:100%;
-            padding:10px;
-            color:white;
-            background-color: #5fb382;
-            text-align: center;
-            text-decoration: none;
-        }
-
-        .carrinho-item{
-            max-width: 1200px;
-            margin: 10px auto;
-        }
-        .carrinho-item p{
-            font-size: 16px;
-            color: black;
-            padding-bottom: 10px;
-            border-bottom: 2px dotted #ccc;
-        }
-    </style>
-
+    <title>Conta</title>
+    <link rel="stylesheet" href="css/carrinho.css">
+    <link href='https://fonts.googleapis.com/css?family=Bayon' rel='stylesheet'>
 </head>
 <body>
-    <h2 class="title">Carrinho com PHP<h2>
-    <div class="carrinho-container">
+    <header>
+        <nav role="navigation">
+            <div id="menuToggle">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+                <ul id="menu">
+                    <a href="pacotes.html"><li>Pacotes &nbsp;<img src="imagens/icons8-sacola-de-compras-50.png"height="25px" width="25px" ></li></a>
+                    <a href="paginainicial.html"><li>Pagina inicial&nbsp;<img src="imagens/icons8-casa-30.png" height="25px" width="25px"></li></a>
+                    <a href="cadastro.html"><li>Cadastros&nbsp;<img src="imagens/icons8-casa-30.png" height="25px" width="25px"></li></a>
+                    <a href="login.html"><li>Login&nbsp;<img src="imagens/icons8-usuário-50.png" height="25px" width="25px"></li></a> 
+                    <a href="suporte.html"><li>Suporte&nbsp;<img src="imagens/icons8-suporte-50.png" height="25px" width="25px" ></li></a>
+                </ul>
+            </div>
+        </nav>
+        <div class="menu">
+            <h1><a href="paginainicial.html">Trailermarine</a></h1>
+            <div class="submarino"></div>
+            <div class="icon">
+                <div class="submarino"><a href="carrinho.html"><img src="imagens/submarino.png" width="80px" height="70px"></a></div>
+                <div class="conta"><a href="user.html"><img src="imagens/icon.png" width="60px" height="50px"></a></div>
+            </div> 
+        </div>
+    </header>
+    <div class="container">
+        <div class="caixona">
 
-    <?php
+        
 
-$items = array(
-    ['nome' => 'Pacote 1 - Bali', 'item' => 'imagens/pacote1.avif', 'preco' => '200'],
-    ['nome' => 'Pacote 2 - Maldivas', 'item' => 'imagens/pacote2.jfif', 'preco' => '300'],
-    ['nome' => 'Pacote 3 - Caribe', 'item' => 'imagens/pacote3.jfif', 'preco' => '400'],
-    ['nome' => 'Pacote 4 - Havaí', 'item' => 'imagens/pacote 4.jfif', 'preco' => '500']
-);
-
-        foreach ($items as $key => $value) {
-    ?>
-        <div class="produto">
-            <img src="<?php echo $value['item']; ?>" />
-            <a href="?adicionar=<?php echo $key  ?>">Adicionar ao carrinho</a>
-        </div><!--produto-->
-
-        <?php
-            }
-        ?>
-
-        </div><!-- carrinho-container-->
-
-        <?php
-
-           if(isset($_GET['adicionar'])){
-                // Vamos adicionar os produtos no carrinho:
-                $idProduto = (int) $_GET['adicionar'];
-                if(isset($items[$idProduto])){
-                    if(isset($_SESSION['carrinho'][$idProduto])){
-                        $_SESSION['carrinho'][$idProduto]['quantidade']++;
-                    } else{
-                        $_SESSION['carrinho'][$idProduto] = array('quantidade'=>1,'nome'=>$items[$idProduto]['nome'],'preco' =>$items[$idProduto]['preco'] );
-                    }
-                    echo '<script>alert("O item foi adicionado ao carrinho.");</script>';
-                    } else{
-                        die('Você não pode adicionar um produto que não existe.');
-                }
-                
-           } 
-
-        ?>
-
-        <h2 class="title"> Carrinho:</h2>
-
-        <?php include('carrinho2.php'); ?>
-    </body>
+        </div>
+    </div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
 </html>
-
-
